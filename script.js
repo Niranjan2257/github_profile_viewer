f67ee5700c833da8831f8851ce7acd45e60d43d7
Niranjan <niranjan2257@gmail.com>
2023-05-14 05:22:28 +0530

    Github profile viewer

---
+++ script.js
@@ -0,0 +1,79 @@
+// DOM elements
+const usernameInput = document.getElementById("username-input");
+const submitButton = document.getElementById("submit");
+
+// Event listener for submit button
+submitButton.addEventListener("click", fetchUserData);
+
+// Function to fetch user data
+function fetchUserData() {
+  const username = usernameInput.value.trim();
+  const apiUrl = `https://api.github.com/users/${username}`;
+
+  // Fetch user profile data
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const profileElement = document.getElementById("profile");
+
+      // Create profile HTML
+      const profileHTML = `
+      <div>
+      <center><img src="${data.avatar_url}" alt="Profile Picture">
+      <h2>${data.name}</h2>
+      <p>${data.login}</p></center>
+      <p>Bio: ${data.bio}</p>
+      <p>Followers: ${data.followers}</p>
+      <p>Following: ${data.following}</p>
+      <p>Public Repositories: ${data.public_repos}</p>
+      `;
+
+      profileElement.innerHTML = profileHTML;
+    })
+    .catch(error => {
+      const errorMessage = document.createElement("p");
+      errorMessage.classList.add("error-message");
+      errorMessage.textContent = "Error fetching profile data.";
+
+      const profileElement = document.getElementById("profile");
+      profileElement.appendChild(errorMessage);
+      console.error(error);
+    });
+
+  // Fetch user repositories
+  fetch(`${apiUrl}/repos`)
+    .then(response => response.json())
+    .then(data => {
+      const repositoriesElement = document.getElementById("repositories");
+
+      // Create repositories HTML
+      let repositoriesHTML = "";
+      data.forEach(repository => {
+        repositoriesHTML += `
+        <div class="repository">
+          <h2>${repository.name}</h2>
+          <p>${repository.description}</p>
+          <p>Language: ${repository.language}</p>
+          <p>Stars: ${repository.stargazers_count}</p>
+          <p>Contributions: ${repository.contributions}</p>
+        </div>
+        `;
+      });
+
+      repositoriesElement.innerHTML = repositoriesHTML;
+    })
+    .catch(error => {
+      const errorMessage = document.createElement("p");
+      errorMessage.classList.add("error-message");
+      errorMessage.textContent = "Error fetching repositories.";
+
+      const repositoriesElement = document.getElementById("repositories");
+      repositoriesElement.appendChild(errorMessage);
+      console.error(error);
+    });
+}
+
+function openMain() {
+  document.getElementById("repositories").style.display ="flex";
+  document.getElementById("profile").style.display ="block";
+}
